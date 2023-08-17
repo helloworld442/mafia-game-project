4184be107446af0f288ba6e67c4f9cdc67c574b5
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-18 07:12:02 +0900

    feat : feat todo head room number text #31

--- src/features/room/component/RoomHead.jsx
+++ src/features/room/component/RoomHead.jsx
@@ -4,6 +4,7 @@ import { Button } from "../../ui";
 const RoomHead = () => {
   return (
     <div className="room-head">
+      <h4 className="room-head-num">현재 방 : 300</h4>
       <Button id="room-head-button" size="medium" primary>
         Add
       </Button>
