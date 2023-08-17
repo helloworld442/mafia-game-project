ef1cc970a581b8b2cded4a89711c81b680d28742
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-18 03:32:13 +0900

    fix : fix room item structure #25

--- src/features/room/component/RoomItem.jsx
+++ src/features/room/component/RoomItem.jsx
@@ -5,10 +5,10 @@ const RoomItem = () => {
     <li className="room-item">
       <h4 className="room-item-id">1</h4>
       <h2 className="room-item-title">채팅 룸입니당</h2>
-      <span className="room-item-info">
+      <div className="room-item-info">
         <h4 className="room-item-full">1 / 8</h4>
         <h4 className="room-item-game start">start</h4>
-      </span>
+      </div>
     </li>
   );
 };
