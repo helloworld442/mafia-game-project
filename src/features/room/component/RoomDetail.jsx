08801cfdc8f9a8e6dc0e1fb923b28ed801aa6aec
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-18 06:03:48 +0900

    feat : feat room detail component style #30

--- src/features/room/component/RoomDetail.jsx
+++ src/features/room/component/RoomDetail.jsx
@@ -1,7 +1,6 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./RoomDetail.scss";
 import classNames from "classnames";
-import { faX } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const RoomDetail = ({ isOpen, data, onClose }) => {
   return (
@@ -12,6 +11,19 @@ const RoomDetail = ({ isOpen, data, onClose }) => {
           <span className="room-detail-back" onClick={onClose}>
             <b>X</b>
           </span>
+          <label htmlFor="title" className="title-label">
+            TTITLE
+          </label>
+          <h4 id="title" className="room-detail-title">
+            {data.title}
+          </h4>
+          <label htmlFor="full" className="full-label">
+            FULL
+          </label>
+          <h4 id="full" className="room-detail-full">
+            {data.full}
+          </h4>
+          <Link className="room-detail-button">JOIN ROOM</Link>
         </>
       )}
     </div>
