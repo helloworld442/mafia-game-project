ec7d26e760ef0525ab202d668cfd6e71fdc1f4b6
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-18 18:30:28 +0900

    fix : fix modal style #37

--- src/features/room/component/RoomDetail.jsx
+++ src/features/room/component/RoomDetail.jsx
@@ -4,43 +4,45 @@ import { Link } from "react-router-dom";
 
 const RoomDetail = ({ isOpen, data, onClose }) => {
   return (
-    <div className={classNames("room-detail", { active: isOpen })}>
-      {isOpen && (
-        <>
-          {/* 상세 페이지 배경 */}
-          <div className="room-detail-background"></div>
+    <>
+      {/* 상세 페이지 배경 */}
+      <div className={classNames("room-detail-background", { active: isOpen })}></div>
 
-          {/* 상세 페이지 고유 번호  */}
-          <span className="room-detail-id">{data.id}</span>
+      <div className={classNames("room-detail-info", { active: isOpen })}>
+        {isOpen && (
+          <>
+            {/* 상세 페이지 고유 번호  */}
+            <span className="room-detail-id">{data.id}</span>
 
-          {/* 상세 페이지 나가기 버튼 */}
-          <span className="room-detail-back" onClick={onClose}>
-            <b>X</b>
-          </span>
+            {/* 상세 페이지 나가기 버튼 */}
+            <span className="room-detail-back" onClick={onClose}>
+              <b>X</b>
+            </span>
 
-          {/* 상세 페이지 제목 라벨 */}
-          <label htmlFor="title" className="title-label">
-            TTITLE
-          </label>
+            {/* 상세 페이지 제목 라벨 */}
+            <label htmlFor="title" className="title-label">
+              TTITLE
+            </label>
 
-          {/* 상세 페이지 제목  */}
-          <h4 id="title" className="room-detail-title">
-            {data.title}
-          </h4>
+            {/* 상세 페이지 제목  */}
+            <h4 id="title" className="room-detail-title">
+              {data.title}
+            </h4>
 
-          {/* 상세 페이지 인원수 라벨 */}
-          <label htmlFor="full" className="full-label">
-            FULL
-          </label>
+            {/* 상세 페이지 인원수 라벨 */}
+            <label htmlFor="full" className="full-label">
+              FULL
+            </label>
 
-          {/* 상세 페이지 인원수 */}
-          <h4 id="full" className="room-detail-full">
-            {data.full}
-          </h4>
-          <Link className="room-detail-button">JOIN ROOM</Link>
-        </>
-      )}
-    </div>
+            {/* 상세 페이지 인원수 */}
+            <h4 id="full" className="room-detail-full">
+              {data.full}
+            </h4>
+            <Link className="room-detail-button">JOIN ROOM</Link>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/features/ui/footer/Footer.jsx
+++ src/features/ui/footer/Footer.jsx
@@ -9,11 +9,17 @@ const Footer = () => {
   const onClickFooterIcon = () => setAcitve(!active);
 
   return (
-    <footer className={classNames("footer-nav", { active })}>
-      {/* footer 열람 트리커 버튼 */}
-      <h2 className="footer-dash-icon" onClick={onClickFooterIcon}>
-        <FontAwesomeIcon icon={faEllipsis} />
-      </h2>
+    <footer className="footer-nav">
+      {/* footer 배경화면 모자이크 처리 */}
+      <div className={classNames("footer-background", { active })}></div>
+
+      {/* footer info 화면 에니매이션 추가 */}
+      <div className={classNames("footer-info", { active })}>
+        {/* footer 열람 트리커 버튼 */}
+        <h2 className="footer-dash-icon" onClick={onClickFooterIcon}>
+          <FontAwesomeIcon icon={faEllipsis} />
+        </h2>
+      </div>
     </footer>
   );
 };
