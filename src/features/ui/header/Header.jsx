786bbdef032df82606ae021c1a722eaf38e26095
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-18 23:05:30 +0900

    fix : delete header setting button #46

--- src/features/ui/header/Header.jsx
+++ src/features/ui/header/Header.jsx
@@ -1,8 +1,6 @@
 import "./Header.scss";
 import { Link } from "react-router-dom";
 import HeaderLogo from "../../../assets/headerlogo.webp";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
@@ -10,11 +8,6 @@ const Header = () => {
       {/* 헤더 홈으로 가는 링크 아이콘*/}
       <Link to="/">
         <img src={HeaderLogo} alt="Mafia" className="header-logo" />
-      </Link>
-
-      {/* 헤더 설정으로 가는 링크 아이콘*/}
-      <Link to="/setting" className="header-text">
-        <FontAwesomeIcon icon={faEllipsisVertical} />
       </Link>
     </header>
   );
