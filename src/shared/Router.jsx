59af43baac68093672aa5b007daf905475a218bb
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-19 13:16:38 +0900

    chore : chore preconnect module

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,24 +1,25 @@
 import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Spinner } from "../features/ui";
-import Home from "../pages/Home";
-import Write from "../pages/Write";
-import SignIn from "../pages/SignIn";
-import SignUp from "../pages/SignUp";
-import Setting from "../pages/Setting";
+
+const Home = lazy(() => import("../pages/Home"));
+const Write = lazy(() => import("../pages/Write"));
+const SignIn = lazy(() => import("../pages/SignIn"));
+const SignUp = lazy(() => import("../pages/SignUp"));
+const Setting = lazy(() => import("../pages/Setting"));
 
 const Router = () => {
   return (
     <BrowserRouter>
-      {/* <Suspense fallback={<Spinner />}> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/write" element={<Write />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/setting" element={<Setting />} />
-      </Routes>
-      {/* </Suspense> */}
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/write" element={<Write />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/setting" element={<Setting />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
